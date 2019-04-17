8580b36846234a38673f40f22aa06c9199152290
Danh Le <dtle82@gmail.com>
2019-04-17 03:23:45 -0700

    built board dynamically with dom insertion

---
+++ main.js
@@ -0,0 +1,74 @@
+var chessboard = document.getElementById("chessboard");
+
+// first row
+chessboard.insertAdjacentHTML('beforeend', `<div class="white">&#9820;</div>
+<div class="black">&#9822;</div>
+<div class="white">&#9821;</div>
+<div class="black">&#9819;</div>
+<div class="white">&#9818;</div>
+<div class="black">&#9821;</div>
+<div class="white">&#9822;</div>
+<div class="black">&#9820;</div>`);
+
+chessboard.insertAdjacentHTML('beforeend', `<div class="black">&#9823;</div>
+<div class="white">&#9823;</div>
+<div class="black">&#9823;</div>
+<div class="white">&#9823;</div>
+<div class="black">&#9823;</div>
+<div class="white">&#9823;</div>
+<div class="black">&#9823;</div>
+<div class="white">&#9823;</div>`);
+
+chessboard.insertAdjacentHTML('beforeend', `<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>`);
+
+chessboard.insertAdjacentHTML('beforeend', `<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>`);
+
+chessboard.insertAdjacentHTML('beforeend', `<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>`);
+
+chessboard.insertAdjacentHTML('beforeend', `<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>`);
+
+chessboard.insertAdjacentHTML('beforeend', `<div class="white">&#9817;</div>
+<div class="black">&#9817;</div>
+<div class="white">&#9817;</div>
+<div class="black">&#9817;</div>
+<div class="white">&#9817;</div>
+<div class="black">&#9817;</div>
+<div class="white">&#9817;</div>
+<div class="black">&#9817;</div>`);
+
+chessboard.insertAdjacentHTML('beforeend', `<div class="black">&#9814;</div>
+<div class="white">&#9816;</div>
+<div class="black">&#9815;</div>
+<div class="white">&#9813;</div>
+<div class="black">&#9812;</div>
+<div class="white">&#9815;</div>
+<div class="black">&#9816;</div>
+<div class="white">&#9814;</div>`);
