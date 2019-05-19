237efd0bb0d94cbc2af665f99006f8e0fd78eeaf
Danh Le <dtle82@gmail.com>
2019-05-19 16:48:08 -0700

    Updates

---
+++ precache-manifest.c4661926735de5ada40b5e08e64ceace.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7579b9ce5c02ec612bbee971861db3ad",
+    "url": "/react-chess/index.html"
+  },
+  {
+    "revision": "93f179888f1270151ed9",
+    "url": "/react-chess/static/css/main.8ad7eeae.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/react-chess/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "93f179888f1270151ed9",
+    "url": "/react-chess/static/js/main.f1f6c28b.chunk.js"
+  },
+  {
+    "revision": "ec2021301a8040aab42c",
+    "url": "/react-chess/static/js/runtime~main.590f6eee.js"
+  },
+  {
+    "revision": "02464a71c93b4ed6a6ed1ed056c18481",
+    "url": "/react-chess/static/media/wood-pattern.02464a71.png"
+  }
+]);
