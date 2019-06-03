b019f7cfe00ffb0a9e6676b40f96ca0cb99b3d13
Danh Le <dtle82@gmail.com>
2019-06-02 23:09:20 -0700

    Updates

--- precache-manifest.26135821ece457c4837f3953a9b4bc8b.js
+++ precache-manifest.26135821ece457c4837f3953a9b4bc8b.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8898a220c3f91bea19c83b9119a11e00",
+    "revision": "1ab342e0924e50e6f7c436c6cbbcb332",
     "url": "/react-chess/index.html"
   },
   {
-    "revision": "148aab9d721f32592262",
-    "url": "/react-chess/static/css/main.65b2270c.chunk.css"
+    "revision": "922a4d833f5711a4f2d9",
+    "url": "/react-chess/static/css/main.8b797912.chunk.css"
   },
   {
     "revision": "72c252c37735d2180df8",
     "url": "/react-chess/static/js/2.c2695c00.chunk.js"
   },
   {
-    "revision": "148aab9d721f32592262",
-    "url": "/react-chess/static/js/main.c54c8a88.chunk.js"
+    "revision": "922a4d833f5711a4f2d9",
+    "url": "/react-chess/static/js/main.901709e8.chunk.js"
   },
   {
     "revision": "ec2021301a8040aab42c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-chess/precache-manifest.2c403fbd38b42ce968a9dad0cf4e7745.js"
+  "/react-chess/precache-manifest.26135821ece457c4837f3953a9b4bc8b.js"
 );
 
 self.addEventListener('message', (event) => {
